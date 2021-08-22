072ad1eb77a62d4d76f6bc165e67a39d9ecf2682
tonytangdev <tony.tang.dev@gmail.com>
2021-08-22 15:41:39 +0200

    menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -6,6 +7,22 @@ module.exports = {
       fontFamily: {
         barlow: "'Barlow', sans-serif",
         fraunces: "'Fraunces', serif"
+      },
+
+      colors: {
+        /* Primary */
+        "soft-red": "var(--soft-red)",
+        "yellow": "var(--yellow)",
+        "dark-desaturated-cyan": "var(--dark-desaturated-cyan)",
+        "dark-blue": "var(--dark-blue)",
+        "dark-moderate-cyan": "var(--dark-moderate-cyan)",
+
+        /* Neutral */
+        "very-dark-desaturated-blue": "var(--very-dark-desaturated-blue)",
+        "very-dark-grayish-blue": "var(--very-dark-grayish-blue)",
+        "dark-grayish-blue": "var(--dark-grayish-blue)",
+        "grayish-blue": "var(--grayish-blue)",
+        "white": "var(--white)"
       }
     },
   },
