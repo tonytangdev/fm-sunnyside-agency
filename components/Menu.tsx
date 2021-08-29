9999f48bd9eaeb8ec0852a6a44b7a2365785e067
tonytangdev <tony.tang.dev@gmail.com>
2021-08-29 14:00:57 +0200

    responsive

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Menu as HeadlessMenu } from '@headlessui/react'
+import { Popover } from '@headlessui/react'
 import MenuSvg from '../public/menu.svg'
 
 interface Props {
@@ -9,15 +9,15 @@ interface Props {
 
 export const Menu = (props: Props) => {
     return (
-        <HeadlessMenu as="div">
-            <HeadlessMenu.Button>
+        <Popover>
+            <Popover.Button>
                 <Image src={MenuSvg} alt="menu" />
-            </HeadlessMenu.Button>
-            <HeadlessMenu.Items className="">
-                <div className="absolute right-0 origin-top-right w-full p-5 mt-4">
-                    <div className="bg-white text-center p-4">
-                        <div className="absolute h-0 w-0 right-5 top-0 origin-top-right inline-block border-white border-b-[25px] border-l-[25px] border-l-transparent"></div>
-                        <HeadlessMenu.Item>
+            </Popover.Button>
+            <Popover.Group as="nav" className="">
+                <Popover.Panel>
+                    <div className="absolute right-0 origin-top-right w-full p-5 mt-4">
+                        <div className="bg-white text-center p-4">
+                            <div className="absolute h-0 w-0 right-5 top-0 origin-top-right inline-block border-white border-b-[25px] border-l-[25px] border-l-transparent"></div>
                             <div className="font-barlow text-dark-grayish-blue my-5">
                                 <Link href="/">
                                     <a>
@@ -25,8 +25,6 @@ export const Menu = (props: Props) => {
                                     </a>
                                 </Link>
                             </div>
-                        </HeadlessMenu.Item>
-                        <HeadlessMenu.Item>
                             <div className="font-barlow text-dark-grayish-blue my-5">
                                 <Link href="/">
                                     <a>
@@ -34,8 +32,6 @@ export const Menu = (props: Props) => {
                                     </a>
                                 </Link>
                             </div>
-                        </HeadlessMenu.Item>
-                        <HeadlessMenu.Item>
                             <div className="font-barlow text-dark-grayish-blue my-5">
                                 <Link href="/">
                                     <a>
@@ -43,8 +39,6 @@ export const Menu = (props: Props) => {
                                     </a>
                                 </Link>
                             </div>
-                        </HeadlessMenu.Item>
-                        <HeadlessMenu.Item>
                             <div className="my-8">
                                 <Link href="/">
                                     <a className="bg-yellow py-3 px-5 rounded-full text-black font-semibold font-fraunces" >
@@ -52,10 +46,10 @@ export const Menu = (props: Props) => {
                                     </a>
                                 </Link>
                             </div>
-                        </HeadlessMenu.Item>
+                        </div>
                     </div>
-                </div>
-            </HeadlessMenu.Items>
-        </HeadlessMenu>
+                </Popover.Panel>
+            </Popover.Group>
+        </Popover>
     )
 }
